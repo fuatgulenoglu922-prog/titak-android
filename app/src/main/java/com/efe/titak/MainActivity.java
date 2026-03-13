1457cdc9c686ee702d8440229fde54b5121b2606
New PC <newpc@example.com>
2026-03-13 09:12:34 +0300

    Add missing Toast import

--- app/src/main/java/com/efe/titak/MainActivity.java
+++ app/src/main/java/com/efe/titak/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.media.projection.MediaProjectionManager;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
